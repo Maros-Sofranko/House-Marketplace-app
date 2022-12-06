96ab2d0c7af90861ee7ff0df48dc41e4598d3df5
Maroš Šofranko <maros.sofranko@metis.academy>
2022-12-06 11:36:44 +0100

    AddNavbar

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Explore() {
+  return (
+    <div>
+      <h1>Explore</h1>
+    </div>
+  );
+}
+
+export default Explore;
